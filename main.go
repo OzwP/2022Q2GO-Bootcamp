5f15a493c94edc3f19f814e725d08c403d0395ed
Oswaldo <>
2022-05-18 18:20:54 -0500

    *changed* search returns specified id instead of all items

--- main.go
+++ main.go
@@ -1,34 +1,21 @@
 package main
 
 import (
-	"encoding/csv"
-	"os"
+	// "strconv"
+	"capstoneProyect/routes"
 
 	"github.com/gofiber/fiber"
 )
 
-func helloWorld(c *fiber.Ctx) {
-	c.Send("Hello World")
-}
-
-func readCSV(c *fiber.Ctx) {
-
-	f, _ := os.Open("data.csv")
-
-	defer f.Close()
-
-	csvReader := csv.NewReader(f)
-	data, _ := csvReader.ReadAll()
-
-	c.Send(data)
-
+func setupRoutes(app *fiber.App) {
+	app.Get("/", routes.Index)
+	app.Get("/getId/:id", routes.ReadId)
 }
 
 func main() {
 	app := fiber.New()
 
-	app.Get("/", helloWorld)
-	app.Get("/readCSV", readCSV)
+	setupRoutes(app)
 
 	app.Listen(3000)
 }
---
+++ routes/routes.go
@@ -0,0 +1,38 @@
+package routes
+
+import (
+	"encoding/csv"
+	_ "fmt"
+	"os"
+
+	"github.com/gofiber/fiber"
+)
+
+func Index(c *fiber.Ctx) {
+	c.Send("Hello World")
+}
+
+func ReadId(c *fiber.Ctx) {
+
+	f, _ := os.Open("data.csv")
+
+	defer f.Close()
+
+	csvReader := csv.NewReader(f)
+	data, _ := csvReader.ReadAll()
+
+	elements := make(map[string]map[string]string)
+	headers := data[0]
+
+	id := c.Params("id")
+
+	for _, element := range data[1:] {
+		elements[element[0]] = map[string]string{}
+		for i, header := range headers {
+			elements[element[0]][header] = element[i]
+		}
+	}
+
+	c.JSON(elements[id])
+
+}
